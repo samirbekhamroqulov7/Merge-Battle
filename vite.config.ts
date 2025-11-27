5e552c604372727d9d4c297392b1ec7bf6b1a8e7
Samirbek Samir <samirhamrakulov763@gmail.com>
2025-11-27 18:17:10 +0500

    Add base path for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-
-// Финальный vite.config.ts
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-  },
-  build: {
-    target: 'esnext', // современный JS
-    outDir: 'build',  // куда будет собираться проект
-    sourcemap: true,  // удобнее дебажить в проде
-  },
-  server: {
-    port: 3000,       // можно поменять, если нужно
-    open: true,       // откроет браузер автоматически
-  },
-});
+import { defineConfig } from 'vite'; 
+import react from '@vitejs/plugin-react-swc'; 
+import path from 'path'; 
+ 
+// Финальный vite.config.ts 
+export default defineConfig({ 
+  base: './', 
+  plugins: [react()], 
+  resolve: { 
+    alias: { 
+      '@': path.resolve(__dirname, './src'), 
+    }, 
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'], 
+  }, 
+  build: { 
+    target: 'esnext', 
+    outDir: 'build', 
+    sourcemap: true, 
+  }, 
+  server: { 
+    port: 3000, 
+    open: true, 
+  }, 
+}); 
