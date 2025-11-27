18a8d079f1ace403984c19cdb1bb9823121196fc
Samirbek Samir <samirhamrakulov763@gmail.com>
2025-11-27 18:41:40 +0500

    Update vite config and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite'; 
-import react from '@vitejs/plugin-react-swc'; 
-import path from 'path'; 
- 
-export default defineConfig({ 
-  plugins: [react()], 
-  resolve: { 
-    alias: { 
-      '@': path.resolve(__dirname, './src'), 
-    }, 
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'], 
-  }, 
-  build: { 
-    target: 'esnext', 
-    outDir: 'build', 
-    sourcemap: true, 
-  }, 
-  server: { 
-    port: 3000, 
-    open: true, 
-  }, 
-}); 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig({
+  base: '/',   // <--- обязательно для Netlify
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  build: {
+    target: 'esnext',
+    outDir: 'build',   // <--- можно оставить
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
