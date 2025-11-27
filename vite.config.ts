5820804158f49406a3e54c00b778e1bce37887ea
Samirbek Samir <samirhamrakulov763@gmail.com>
2025-11-27 18:28:30 +0500

    Remove base path for correct asset loading

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'; 
 import path from 'path'; 
  
-// Финальный vite.config.ts 
 export default defineConfig({ 
-  base: './', 
   plugins: [react()], 
   resolve: { 
     alias: { 
